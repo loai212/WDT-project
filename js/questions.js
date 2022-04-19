f64e47c8d96d99f4a1a95cd411adc6540f8750e9
loai212 <88862158+loai212@users.noreply.github.com>
2022-04-19 21:14:10 +0200

    WDT-Project Code(French website)

---
+++ js/questions.js
@@ -0,0 +1,73 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the word for Good Morning in french ?",
+    answer: "Bonjour",
+    options: [
+      "Gauche",
+      "Au revoir",
+      "Bonsoir",
+      "Bonjour"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the correct translation for telephone?",
+    answer: "un téléphone",
+    options: [
+      "un téléphone",
+      "un parapluie",
+      "de l'eau",
+      "une bicyclette"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How do you say Goodbye?",
+    answer: "Au revoir",
+    options: [
+      "Au revoir",
+      "Bonsoir",
+      "Où est la plage?",
+      "Gauche"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the correct translation for right?",
+    answer: "trois",
+    options: [
+      "trois",
+      "Bonsoir",
+      "Au revoir",
+      "Au revoir",
+
+    ]
+  },
+    {
+    numb: 5,
+    question: "How do you say water?",
+    answer: "du lait",
+    options: [
+      "du lait",
+      "de l'eau",
+      "Désolé",
+      "un café"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
